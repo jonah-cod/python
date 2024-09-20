3cc1a29abfbbcd9859835cc04e109d82ce734c8a
Jonathan Mwaniki <idolearndaily@gmail.com>
2024-09-20 11:08:47 +0300

    relative imports

--- file_structure/compressed/bzipped.py
+++ file_structure/compressed/bzipped.py
@@ -1,9 +1,6 @@
 import bz2
-import sys
-
+from file_structure.utils import writer
 opener = bz2.open
 
 if __name__ == "__main__":
-    f = bz2.open(sys.argv[1], mode='wt');
-    f.write(" ".join(sys.argv[2:]))
-    f.close()
+    writer.main(opener)
